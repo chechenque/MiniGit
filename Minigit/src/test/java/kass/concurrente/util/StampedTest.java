f52bfa961d45a10e779080a164c4a3913822662b
janet1204 <janet1204@grr.fciencias.unam.mx>
2024-05-03 02:17:07 -0600

    Tests de segundos constructores

--- Minigit/src/test/java/kass/concurrente/util/StampedTest.java
+++ Minigit/src/test/java/kass/concurrente/util/StampedTest.java
@@ -9,10 +9,9 @@ import static org.junit.jupiter.api.Assertions.*;
 public class StampedTest {
 
     @Test
-    public void testStampedSnap() {
+    void testStampedSnap() {
         
         StampedSnap<String> stampedSnap = new StampedSnap<>("Inicial");
-        
         
         assertEquals("Inicial", stampedSnap.getValue());
         assertEquals(0, stampedSnap.getStamp());
@@ -23,14 +22,21 @@ public class StampedTest {
         String[] snap = {"Cambio1", "Cambio2"};
         stampedSnap.setSnap(snap);
         
-        
         assertEquals("Modificado", stampedSnap.getValue());
         assertEquals(1, stampedSnap.getStamp());
         assertArrayEquals(snap, stampedSnap.getSnap());
+
+        String[] snap2 = {"Estado1", "Estado2"};
+        StampedSnap<String> stampedSnap2 = new StampedSnap<>(1, "Nuevo", snap2);
+        
+        assertEquals("Nuevo", stampedSnap2.getValue());
+        assertEquals(1, stampedSnap2.getStamp());
+        assertArrayEquals(snap2, stampedSnap2.getSnap());
+
     }
 
     @Test
-    public void testStampedValue() {
+    void testStampedValue() {
         
         StampedValue<Integer> stampedValue = new StampedValue<>(10);
         
@@ -45,5 +51,12 @@ public class StampedTest {
     
         assertEquals(20, (int) stampedValue.value());
         assertEquals(1, stampedValue.getStamp());
+
+        StampedValue<Integer> stampedValue2 = new StampedValue<>(2, 5);
+        
+        assertEquals(5, (int) stampedValue2.value());
+        assertEquals(2, stampedValue2.getStamp());
+        //assertEquals(max(stampedValue, stampedValue2), stampedValue2);
+
     }
 }
