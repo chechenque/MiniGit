c790f966deca3d8fccdfb8e56e30eb4fce7cc479
albert <natanael_pi@ciencias.unam.mx>
2024-05-04 13:04:18 -0600

    Stash test: prototipo III (antes de otro issue bug)

--- Minigit/src/test/java/kass/concurrente/stash/StashTest.java
+++ Minigit/src/test/java/kass/concurrente/stash/StashTest.java
@@ -1,5 +1,7 @@
 package kass.concurrente.stash;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
@@ -10,6 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -23,38 +26,80 @@ import kass.concurrente.modelos.Carpeta;
  */
 class StashTest {
 
-    private Stash stash;
-    private Random rnd;
-    private final String rutaTest = "ruta" + File.separator + 
+    private static Stash stash;
+    private static Random rnd;
+    private static List<Archivo> archivos; 
+    private static Carpeta carpetaTest;
+    private final String ramaNombre = "ramaTest";
+    private static final String carpetaNombre = "carpetaTest";
+    private static final String rutaTest = "ruta" + File.separator + 
                                     "para" + File.separator + "test";
     private final String rutaStash = System.getProperty("user.dir") 
                                      + File.separator + "stashes";
+
     
-    @BeforeEach
-    void setUp() {
-        this.stash = new Stash();
+    @BeforeAll
+    static void setUp() {
+        stash = new Stash();
         rnd = new Random();
+        archivos = generaArchivos(true);
+        carpetaTest = generaCarpeta(archivos);
+    }
+
+    private static List<Archivo> generaArchivos(boolean vacios) {
+        int numArchivos = rnd.nextInt(7) + 1; // Genera un número aleatorio entre 1 y 7
+        List<Archivo> result = new ArrayList<>();
+    
+        for(int i = 0; i < numArchivos; i++){
+            Archivo archTemp = new Archivo(rutaTest, "archivo" + i);
+            result.add(archTemp);
+        }
+
+        return result;
+    }
+
+    private static Carpeta generaCarpeta(List<Archivo> archivos) { 
+        Carpeta dirTemp = new Carpeta(rutaTest, carpetaNombre);
+        dirTemp.setArchivos(archivos);
+
+        return dirTemp;
     }
 
     @Test
     void guardarStashTest() {
-    int numArchivos = rnd.nextInt(7) + 1; // Genera un número aleatorio entre 1 y 7
-    List<Archivo> archivos = new ArrayList<>();
-
-    for(int i = 0; i < numArchivos; i++){
-        Archivo archTemp = new Archivo(rutaTest, "archivo" + i);
-        archivos.add(archTemp);
+        try {
+            stash.guardarStash(ramaNombre, carpetaTest);
+        } catch (IOException | NullPointerException npe) {
+            fail("El stash debió ser guardado correctamente.");
+        }
     }
-    Carpeta dirTemp = new Carpeta(rutaTest, "carpetaTest");
-    dirTemp.setArchivos(archivos);
 
-    try {
-        stash.guardarStash("ramaTest", dirTemp);
-    } catch (IOException | NullPointerException npe) {
-        fail("El stash debió ser guardado correctamente.");
+    @Test 
+    void getNombresTest() {
+        String stashes = stash.getNombres(ramaNombre);
+        String[] nombres = stashes.trim().split("\n");
+        if (nombres.length != archivos.size())
+            fail(); // Tentativo: si no le prestamos atencion a archivos vacios.
+        for (int i = 0; i < nombres.length; i++) 
+            assertEquals(nombres[i], archivos.get(i).getNombre());
     }
-}
 
+    @Test
+    void getUltimoStashTest() {
+        assertNotEquals("", stash.getUltimoStash(ramaNombre));        
+    }
 
-    
+    @Test
+    void getRutaStashTest() {
+        String stashInex = "inexistente";
+        //assertEquals(rutaStash + File.separator + ramaNombre, stash.getRutaStash(ramaNombre, stashInex));
+        assertEquals("", stash.getRutaStash(ramaNombre, stashInex));
+    }
+
+    @Test
+    void borraStashTest() {
+        String stashInex = "inexistente";
+        boolean result = stash.borraStash(ramaNombre, stashInex);
+        assertFalse(result);
+    }
 }
